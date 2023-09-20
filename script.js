5d7014e22375a8c0c43c95edf1b5413905640630
Maria Eduarda <mariaecarvalho14@gmail.com>
2023-09-20 13:30:48 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  //  if(html.classList.contains('light')) {
+  //   html.classList.remove('light')
+  // } else {
+  //   html.classList.add('light')
+  //}
+  // se contém e for verdadeiro, ele remove o light, se for falso, ele adiciona o light.
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a img
+  if (html.classList.contains("light")) {
+    // se tiver light mode, add img light
+    img.setAttribute("src", "./assets/assets/avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a img normal
+    img.setAttribute("src", "./assets/assets/avatar.png")
+  }
+}
